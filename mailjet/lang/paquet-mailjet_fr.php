6d24f1f3ce0a48e0e8d8ffa56200bd966e61bde9
FerhanGit <fismailov@mailjet.com>
2015-02-23 11:13:06 +0200

    * text correction

--- mailjet/lang/paquet-mailjet_fr.php
+++ mailjet/lang/paquet-mailjet_fr.php
@@ -10,7 +10,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// F
 	'mailjet_description' => 'Permet d\'envoyer tous vos emails à travers Mailjet, ainsi que de gérer vos contacts depuis l\'interface d\'aministration de Spip',
 	'mailjet_nom' => 'Mailjet',
-	'mailjet_slogan' => 'Anvoyer vos courrier par votre compte Mailjet'
+	'mailjet_slogan' => 'Envoyez vos courriers par votre compte Mailjet'
 );
 
 ?>
